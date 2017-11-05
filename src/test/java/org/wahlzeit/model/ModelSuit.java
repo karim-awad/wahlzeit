2cbd2aa86dc11223d4cbb243d816d9559d81170e
Karim Awad <karim-awad@outlook.com>
2017-11-05 13:59:19 +0100

    added two missing testcases to modelsuit

--- src/test/java/org/wahlzeit/model/ModelSuit.java
+++ src/test/java/org/wahlzeit/model/ModelSuit.java
@@ -6,8 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 import org.wahlzeit.model.persistence.PersistenceSuit;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PersistenceSuit.class, AccessRightsTest.class, FlagReasonTest.class, GenderTest.class, GuestTest.class, PhotoFilterTest.class,
-		TagsTest.class, UserStatusTest.class, ValueTest.class })
+@SuiteClasses({ PersistenceSuit.class, AccessRightsTest.class,CoordinateTest.class, FlagReasonTest.class, GenderTest.class, GuestTest.class, PhotoFilterTest.class,
+		LocationTest.class, TagsTest.class, UserStatusTest.class, ValueTest.class })
 
 public class ModelSuit {
 
